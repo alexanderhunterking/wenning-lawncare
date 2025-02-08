bb392e0527c0a185c6ac511e5045d6f51514c970
Alexander King <alexanderhunterking@gmail.com>
2025-02-08 17:47:02 -0500

    added the logo in

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
     <nav :class="['navbar', 'navbar-expand-lg', theme === 'dark-mode' ? 'navbar-dark bg-dark-green' : 'navbar-light bg-light-green']">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Wenning Lawncare & Landscaping</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#"><img :src="logoSrc" alt="Wenning Lawncare & Landscaping" class="navbar-logo"></a>
+        <button class="navbar-toggler" type="button" @click="toggleTheme" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -26,26 +26,49 @@
         </div>
       </div>
     </nav>
-  </template>
-  
-  <script>
-  export default {
-    name: 'NavBar',
-    props: {
-      theme: {
-        type: String,
-        default: 'light-mode'
-      }
+</template>
+
+<script>
+import darkLogo from '../assets/darkmodelogo.png';
+import lightLogo from '../assets/lightmodelogo.png';
+
+export default {
+  name: 'NavBar',
+  props: {
+    theme: {
+      type: String,
+      default: 'dark-mode'
+    }
+  },
+  data() {
+    return {
+      darkLogo,
+      lightLogo
+    };
+  },
+  computed: {
+    logoSrc() {
+      return this.theme === 'dark-mode' ? this.darkLogo : this.lightLogo;
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.$emit('toggle-theme');
     }
   }
-  </script>
-  
-  <style scoped>
-  .bg-light-green {
-    background-color: #d4edda;
-  }
-  
-  .bg-dark-green {
-    background-color: #155724;
-  }
-  </style>
+}
+</script>
+
+<style scoped>
+.bg-light-green {
+  background-color: #d4edda;
+}
+
+.bg-dark-green {
+  background-color: #155724;
+}
+.navbar-logo {
+  height: 40px;
+  width: auto;
+}
+</style>
