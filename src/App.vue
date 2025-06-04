478624bb68dfdac27c53ff640f2a94c18d666190
Alexander King <alexanderhunterking@gmail.com>
2025-06-03 20:08:25 -0400

    added services/gallery/contact sections

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@
         <section class="py-5">
           <div class="container about-section" id="about-section" :class="[theme]">
             <div class="row align-items-center gx-4">
-              <div class="col-md-5">
+              <div class="col-5 justify-content-center">
                 <div class="ms-md-2 ms-lg-5 mb-4 mt-4">
-                  <img class="img-fluid rounded-3" :src="aboutImage">
+                  <img class="img-fluid rounded-3 " :src="aboutImage">
                 </div>
               </div>
               <div class="col-md-6 offset-md-1">
@@ -88,7 +88,7 @@
     </div>
     <!-- services section -->
      <div class="row mb-3">
-      <section class="py-5">
+      <section class="py-5" id="services-section">
 	<div class="container">
 		<div class="row justify-content-center text-center mb-3 mb-md-5">
 			<div class="col-lg-8 col-xxl-7">
@@ -156,6 +156,90 @@
 	</div>
 </section>
      </div>
+     <!-- gallery section -->
+      <div class="row mb-3">
+        <section class="py-5" id="gallery-section">
+	<div class="container">
+		<div class="row justify-content-center text-center mb-2 mb-lg-4">
+			<div class="col-12 col-lg-8 col-xxl-7 text-center mx-auto">
+				<h2 class="display-5 fw-bold">Our Gallery</h2>
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta harum ipsum venenatis metus sem veniam eveniet aperiam suscipit.</p>
+			</div>
+		</div>
+		<div class="row py-3 align-items-center">
+			<div class="col-md-6 mt-md-0 mt-4">
+				<div class="mb-5 mb-lg-3"><img class="img-fluid" :src="img2"></div>
+			</div>
+			<div class="col-md-6 ps-md-5">
+				<div class="mb-5 mb-lg-3">
+					<h4>A Catchy Title</h4>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente accusantium nulla, sequi cupiditate exercitationem sit incidunt rerum explicabo. Soluta beatae esse praesentium exercitationem vero harum pariatur adipisci qui veritatis est!</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta harum ipsum venenatis metus sem veniam eveniet aperiam suscipit.</p><a class="btn btn-lg btn-primary" href="">View Gallery</a>
+				</div>
+			</div>
+		</div>
+		<div class="row mt-2">
+			<div class="col-lg-3 col-md-6">
+				<div class="mb-3 mb-lg-0"><img alt="" class="img-fluid" :src="img7"></div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="mb-3 mb-lg-0"><img alt="" class="img-fluid" :src="img5"></div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="mb-3 mb-lg-0"><img alt="" class="img-fluid" :src="img4"></div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="mb-3 mb-lg-0"><img alt="" class="img-fluid" :src="img6"></div>
+			</div>
+		</div>
+	</div>
+</section>
+      </div>
+      <!-- contact section -->
+       <div class="row mb-3">
+        <section class="py-5">
+	<div class="container-fluid p-lg-0">
+		<div class="row g-0 align-items-center">
+			<div class="col-lg-6">
+				<div class="col-lg-8 mx-auto">
+					<span class="text-muted">Let's Talk</span>
+					<h2 class="display-5 fw-bold">Contact Us</h2>
+					<p class="lead">Reach out to us! It doesn't matter whether you just want some fresh mulch, mowing, or a complete landscaping overhaul, </p>
+					<p class="lead">Phone: (765) 571-9457</p>
+          <p class="lead">Email: wenninglandscape@gmail.com</p>
+          <form>
+						<div class="row">
+							<div class="col-md-12">
+								<div class="mb-3">
+									<input class="form-control bg-light" placeholder="Your name" type="text">
+								</div>
+							</div>
+							<div class="col-md-12">
+								<div class="mb-3">
+									<input class="form-control bg-light" placeholder="Your email" type="text">
+								</div>
+							</div>
+							<div class="col-md-12">
+								<div class="mb-3">
+									<textarea class="form-control bg-light" placeholder="Your message" rows="4"></textarea>
+								</div>
+							</div>
+							<div class="col-md-5">
+								<div class="d-grid">
+									<button class="btn btn-primary" type="submit">Send message</button>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="mt-5 mt-lg-0"><img alt="" class="img-fluid" :src="map"></div>
+			</div>
+		</div>
+	</div>
+</section>
+       </div>
     <Footer @toggle-theme="toggleTheme" :theme="theme"/>
   </div>
 </template>
@@ -165,6 +249,14 @@ import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
 import bannerImage from './assets/JordanWenning-2930_Original.JPG'
 import aboutImage from './assets/JordanWenning-2753_Original.JPG'
+import img1 from './assets/IMG_2143.png'
+import img2 from './assets/IMG_5193.png'
+import img3 from './assets/IMG_5818.png'
+import img4 from './assets/IMG_6828.png'
+import img5 from './assets/IMG_6845.png'
+import img6 from './assets/IMG_7338.png'
+import img7 from './assets/IMG_9873.png'
+import map from './assets/map.png'
 
 export default {
   name: 'App',
@@ -176,7 +268,15 @@ export default {
     return {
       theme: 'dark-mode',
       bannerImage,
-      aboutImage
+      aboutImage,
+      img1,
+      img2,
+      img3,
+      img4,
+      img5,
+      img6,
+      img7,
+      map
     }
   },
   methods: {
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -20,6 +20,9 @@
               <a class="nav-link" href="#">Services</a>
             </li>
             <li class="nav-item">
+              <a class="nav-link" href="#gallery-section">Gallery</a>
+            </li>
+            <li class="nav-item">
               <a class="nav-link" href="#">Contact</a>
             </li>
           </ul>
